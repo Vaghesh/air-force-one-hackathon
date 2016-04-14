2d82b5beac2e70e6bd9179631bacd83a37142dcc
Vaghesh Patel <vaghesh.patel@intel.com>
2016-04-13 17:38:44 -0700

    Added Sync function to execute shell command

--- edison/main.js
+++ edison/main.js
@@ -2,7 +2,7 @@
 //Set up our GPIO input for pin 5 on the Arduino breakout board
 var mraa = require('mraa');
 var groveSensor = require('jsupm_grove');
-var execSync = require('exec-sync');
+var exec = require('sync-exec');
 
 var motionSensor = new mraa.Gpio(5);
 var button = new groveSensor.GroveButton(6);
@@ -12,14 +12,13 @@ mylcd.setColor(0,0,0);
 
 var armed = false;
 
-
 //Set up our node mailer
 var nodemailer = require("nodemailer");
 var smtpTransport = nodemailer.createTransport({
   service: "Gmail",
 	auth: {
 		user: "vagheshp@gmail.com",
-		pass: "xxxxxx"
+		pass: "nisargdesai"
 	}
 });
 
@@ -28,53 +27,40 @@ function writeLCD(lcdtext,r,g,b){
   mylcd.clear();
   mylcd.setColor(r,g,b);
   mylcd.write(lcdtext);
-
 }
 
 
 
 //Here is our sensing function:
 
-function armedActivity()
-{
+function armedActivity(){
+
 	//Read our sensor
 	var motionSensorTriggered = motionSensor.read();
-  console.log("Waiting for Motion Senor")
 
 	//Do stuff if our sensor is read HIGH
 	if(motionSensorTriggered){
-		
-    execSync("wget -O /home/root/image.jpg http://webcam.local:9000/?action=snapshot", 5000);
+		exec("wget -O /home/root/image.jpg http://webcam.local:9000/?action=snapshot", 5000);
 		//Send our email message
 		smtpTransport.sendMail({
 			from: "Vaghesh Patel <vagheshp@gmail.com>",
 			to: "Vaghesh Patel <vagheshp@gmail.com>",
 			subject: "Possible Intruder Alert",
-      attachments:[{   // file on disk as an attachment
-            filename: '123.jpg',
-            path: '/home/root/123.jpg' // stream this file
-        }],
+      			attachments:[{   // file on disk as an attachment
+            			filename: '123.jpg',
+            			path: '/home/root/123.jpg' // stream this file
+        		}],
 			text: "Hey! Something activated your motion sensor!"
 		}, function(error, response){ //Send a report of the message to the console
 			if(error){
 				console.log(error);
 			}else{
-        writeLCD("Message Sent",255,255,255)
+        			writeLCD("Message Sent",255,255,255)
 				console.log("Message sent: " + response.message);
 			}
 			smtpTransport.close();
 		});
 
-		//We don't want 5 million emails, so we want to wait a few seconds
-		//(in this case, 30 seconds) before sending another email. The timeout
-		//is in milliseconds.  So, for 1 minute, you would use 60000.
-		setTimeout(armedActivity, 60000);
-
-	}else{
-		//Our motion sensor wasn't triggered, so we don't need to wait as long.
-		// 1/10 of a second seems about right and allows our Edison to do other
-		// things in the background.
-		setTimeout(armedActivity, 100);
 	}
 }
 
@@ -89,7 +75,7 @@ function monitoring()
     console.log("Armed")
     writeLCD("Armed",255,0,0)
     armed = true;
-    armedActivity();
+    setTimeout(armedActivity(),10000);
   }
   else if (armed == true && button.value()) {
     console.log("Disarmed")

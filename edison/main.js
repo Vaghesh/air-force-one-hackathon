cda16fe66cdf42480d7de6bba5c515782c84cc95
Vaghesh Patel <vaghesh@gmail.com>
2016-07-19 16:08:11 -0700

    Updated the Code for extra questions

--- edison/main.js
+++ edison/main.js
@@ -1,40 +1,44 @@
 
-//Set up our GPIO input for pin 5 on the Arduino breakout board
+// Required Packages
 var mraa = require('mraa');
 var groveSensor = require('jsupm_grove');
+var sensorModule = require('jsupm_ttp223');
 var exec = require('sync-exec');
 var Client = require("ibmiotf");
+var nodemailer = require("nodemailer");
 
-var motionSensor = new mraa.Gpio(5);
+
+
+// Initialization of GPIO Pins, Variables and Configurations for Email and Cloud Connectivity
+var PIRSensor = new mraa.Gpio(5);
+PIRSensor.dir(mraa.DIR_IN);
+var touchSensor = new sensorModule.TTP223(4);
 var button = new groveSensor.GroveButton(6);
 var mylcd = new (require("jsupm_i2clcd").Jhd1313m1)(6, 0x3E, 0x62);
-motionSensor.dir(mraa.DIR_IN);
-writeLCD("Not Monitoring", 0,0,255);
-
-
 var monitoring = false;
 var intervalId;
 
-//Set up our node mailer
-var nodemailer = require("nodemailer");
-// For Google account, you need to turn on less secure option from your account.
-var smtpTransport = nodemailer.createTransport({
+var smtpTransport = nodemailer.createTransport({	// Email Configuration
   service: "Gmail",
 	auth: {
 		user: "airforceonehackathon@gmail.com",
 		pass: "xxxxxxxx"
 	}
 });
-var config = {
+var config = {					// Cloud Connectivity Configuration
     "org" : "quickstart",
     "id" : "xxxxxxxxxx",
     "type" : "iotsensor"
 };
 
-var deviceClient = new Client.IotfDevice(config);
+var deviceClient = new Client.IotfDevice(config);	// Cloud Connectivity Iniitialization
 deviceClient.connect();
 
 
+
+
+
+// Function to write at LCD. Pass parameter LCD text and Red, Green, Blue Colors
 function writeLCD(lcdtext,r,g,b){
   mylcd.clear();
   mylcd.setColor(r,g,b);
@@ -43,34 +47,51 @@ function writeLCD(lcdtext,r,g,b){
 
 
 
-//Here is our sensing function:
 
+
+
+// Function to Send Email 
+function sendEmail()
+{
+	exec("wget -O /home/root/image.jpg http://localhost:9000/?action=snapshot", 5000);
+	smtpTransport.sendMail({
+		from: "Air Force One Hackathon <airforceonehackathon@gmail.com>",
+		to: "Your Name <xxxxxx@gmail.com>",
+		subject: "Possible Intruder Alert",
+      		attachments:[{   // file on disk as an attachment
+            		filename: 'image.jpg',
+            		path: '/home/root/image.jpg' // stream this file
+        	}],
+		text: "Hey! Something activated your motion sensor!"
+	}, function(error, response){ //Send a report of the message to the console
+		if(error){
+			console.log(error);
+		}else{
+			console.log("Email Sent");
+		}
+		smtpTransport.close();
+	});	
+}
+
+// Function to Read PIR value
+function readPIR(){
+	return PIRSensor.read();	
+}
+
+// Function to Read Button Value 
+function readTouchSensor(){
+	return touchSensor.isPressed();
+}
+
+
+
+
+// Function  to start Monitoring your cube
 function monitoringActivity(){
-
-	//Read our sensor
-	var motionSensorTriggered = motionSensor.read();
-	//Do stuff if our sensor is read HIGH
-	if(motionSensorTriggered){
+	var triggered = readPIR()
+	if(triggered){
 		deviceClient.publish("status","json",'{"d" : { "pir" : 1}}');
-		exec("wget -O /home/root/image.jpg http://localhost:9000/?action=snapshot", 5000);
-		//Send our email message
-		smtpTransport.sendMail({
-			from: "Air Force One Hackathon <airforceonehackathon@gmail.com>",
-			to: "Your Name <xxxxxx@gmail.com>",
-			subject: "Possible Intruder Alert",
-      			attachments:[{   // file on disk as an attachment
-            			filename: 'image.jpg',
-            			path: '/home/root/image.jpg' // stream this file
-        		}],
-			text: "Hey! Something activated your motion sensor!"
-		}, function(error, response){ //Send a report of the message to the console
-			if(error){
-				console.log(error);
-			}else{
-				console.log("Email Sent");
-			}
-			smtpTransport.close();
-		});
+		sendEmail();
 	}
 	else{
 		deviceClient.publish("status","json",'{"d" : { "pir" : 0}}');
@@ -78,10 +99,7 @@ function monitoringActivity(){
 	return;
 }
 
-
-
-//Start sensing!
-
+// Loop to check that 
 function periodicActivity()
 {
   if (monitoring === false && button.value()){
@@ -93,10 +111,12 @@ function periodicActivity()
   else if (monitoring === true && button.value()) {
     console.log("Not Monitoring");
     writeLCD("Not Monitoring",0,0,255);
-	clearInterval(intervalId);
+    clearInterval(intervalId);
     monitoring = false;
   }
 }
 
+
+writeLCD("Initializing", 0,0,255);
 console.log("Press button to Monitor your cube");
 setInterval(periodicActivity,1000);
